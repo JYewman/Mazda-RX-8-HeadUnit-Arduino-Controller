8d169de017d6d989b78efd030a47914094086f31
JYewman <joshua.yewman@digital.mod.uk>
2024-04-06 15:47:23 +0100

    Head_Unit Control update

--- RX8_HU_CTRL/RX8_HU_CTRL.ino
+++ RX8_HU_CTRL/RX8_HU_CTRL.ino
@@ -8,165 +8,360 @@
  * GNU GPLv3
  */
 
-#define DATA_PIN 2
+// Configuration
 
-volatile uint8_t buff = 0;           // we only need the first 2 nibbles of any given message
-volatile uint8_t buffPos = 0;        // keeps track of which bit we're on
-volatile unsigned long int lowTime;  // keeps track of how long the bus was low
-volatile unsigned long int highTime; // keeps track of how long the bus was high
+#define ENABLE_DEBUG_OUTPUT
 
+// Depended parameters
+
+#define BIT_LOW_LEVEL_DURATION_MIN (1400) // value in us
+#define BIT_LOW_LEVEL_DURATION_MAX (2000) // value in us
+
+#ifdef ENABLE_DEBUG_OUTPUT
+#define DEBUG_PRINT(...)           \
+    if (Serial)                    \
+    {                              \
+        Serial.print(__VA_ARGS__); \
+    }
+#else
+#define DEBUG_PRINT(...)
+#endif
+
+// Constants
+#define TYPE_IO_PIN_INPUT_MODE (INPUT_PULLUP)
+#define TYPE_IO_PIN 8
+
+#define RX_TIMEOUT_MS 12U
+#define IN_BUFFER_SIZE 96U
+
+#define NIBBLE_RESET_BIT_POS 0x08
+
+/* message mustbe aligned to bytes*/
+typedef struct rxMessage
+{
+    uint8_t target;
+    uint8_t command;
+    uint8_t data[];
+} rxMessage_t;
+
+typedef enum rxMessageTarget_e
+{
+    Target_TapeDesk = 0x00,
+    Target_Unknown = 0x01,
+    Target_CDDesk = 0x03,
+    Target_CDChangerExt = 0x05,
+    Target_CDChangerUpper = 0x06,
+    Target_MDDesk = 0x07,
+    Target_BaseUnit = 0x08
+} rxMessageTarget_t;
+
+typedef enum rxMessageCommand_e
+{
+    Command_Control = 0x01,
+    Command_AnyBodyHome = 0x08,
+    Command_WakeUp = 0x09
+} rxMessageCommand_t;
+
+typedef enum rxMessageSubCommand_e
+{
+    SubCommand_Playback = 0x01,
+    SubCommand_SeekTrack = 0x03,
+    SubCommand_SetConfig = 0x04
+} rxMessageSubCommand_t;
+
+typedef enum SubConmmandPlayback_e
+{
+    Playback_Play = 0x01,
+    Playback_FF = 0x04,
+    Playback_REW = 0x08,
+    Playback_Stop = 0x60
+} SubConmmandPlayback_t;
+
+typedef enum SubCommandSetConfig_e
+{
+    SetConfig_RepeatMode = 0x01,
+    SetConfig_RandomMode = 0x02,
+    SetConfig_FastForwarding = 0x10,
+    SetConfig_FastRewinding = 0x20
+} SubCommandSetConfig_t;
+
+// data present in nibbles, byte equal nibble
+// Wakeup notification
+const uint8_t TAPECMD_POWER_ON[] = {0x08, 0x08, 0x01, 0x02}; // Wake up notification
+// Status messages: {target, command(status), arg1, arg2, checksum}
+const uint8_t TAPECMD_STOPPED[] = {0x08, 0x09, 0x00, 0x0C, 0x0E}; // 0 - Stopped, C - not use desk
+const uint8_t TAPECMD_PLAYING[] = {0x08, 0x09, 0x04, 0x01, 0x05}; // 4 - Playing, 1 - tape in use
+const uint8_t TAPECMD_SEEKING[] = {0x08, 0x09, 0x05, 0x01, 0x06}; // 5 - seeking, 1 - tape in use
+// Detailed status  {target, command(det. status), arg1, arg2, arg3, arg4, arg5, arg6, checksum}
+const uint8_t TAPECMD_CASSETE_PRESENT[] = {0x08, 0x0B, 0x09, 0x00, 0x04, 0x00, 0x00, 0x0C, 0x03};
+const uint8_t TAPECMD_PLAYBACK[] = {0x08, 0x0B, 0x09, 0x00, 0x04, 0x00, 0x00, 0x01, 0x00};
+const uint8_t TAPECMD_RANDOM_PLAY[] = {0x08, 0x0B, 0x09, 0x00, 0x06, 0x00, 0x00, 0x01, 0x0E};
+const uint8_t TAPECMD_REPEAT_PLAY[] = {0x08, 0x0B, 0x09, 0x00, 0x05, 0x00, 0x00, 0x01, 0x0F};
+const uint8_t TAPECMD_FAST_REWIND[] = {0x08, 0x0B, 0x09, 0x03, 0x04, 0x00, 0x01, 0x01, 0x0E};
+const uint8_t TAPECMD_FAST_FORWARD[] = {0x08, 0x0B, 0x09, 0x02, 0x04, 0x00, 0x01, 0x01, 0x0D};
+
+static uint8_t inNibblesBuffer[IN_BUFFER_SIZE] = {0U};
+static uint8_t nibblesReceived = 0;
+static uint8_t biteShiftMask = NIBBLE_RESET_BIT_POS;
+static uint32_t rx_time_us = 0;
+static uint32_t rx_time_ms = 0;
 void setup()
 {
-    // set up pins
-    pinMode(DATA_PIN, INPUT);
-    attachInterrupt(0, isr, CHANGE); // attach the interrupt
+    pinMode(TYPE_IO_PIN, TYPE_IO_PIN_INPUT_MODE);
+    attachInterrupt(digitalPinToInterrupt(TYPE_IO_PIN), collectInputData, CHANGE);
 
-    interrupts(); // enable interrupts
+#ifdef ENABLE_DEBUG_OUTPUT
     Serial.begin(9600);
+#endif
+
+    DEBUG_PRINT("Init....\r\n");
 }
 
 void loop()
 {
-    if (buffPos > 7)
-    {                       // if full command is ready
-        detachInterrupt(0); // don't want interrupt firing when trying to send data
-        if (buff == 0x08)
-        { // Check if Tape/MD has been called
-            delay(10);
-            // Power on notification
-            sendNibble(0x8); // for head unit
-            sendNibble(0x8); // wakeup notification command
-            sendNibble(0x1); // wakeup notification data
-            sendNibble(0x2); // checksum
-            delay(10);
-            // Casette present
-            sendNibble(0x8); // for head unit
-            sendNibble(0xB); // detailed status
-            sendNibble(0x9); // data
-            sendNibble(0x0); // data
-            sendNibble(0x4); // data
-            sendNibble(0x0); // data
-            sendNibble(0x0); // data
-            sendNibble(0xC); // data
-            sendNibble(0x3); // checksum
-            Serial.println("Tape/MD Check");
-        }
 
-        else if (buff == 0x09)
-        { // Init Tape/MD
-            delay(10);
-            // Casette present
-            sendNibble(0x8); // for head unit
-            sendNibble(0xB); // detailed status
-            sendNibble(0x9); // data
-            sendNibble(0x0); // data
-            sendNibble(0x4); // data
-            sendNibble(0x0); // data
-            sendNibble(0x0); // data
-            sendNibble(0xC); // data
-            sendNibble(0x3); // checksum
-            delay(10);
-            // Tape Playing
-            sendNibble(0x8); // for head unit
-            sendNibble(0x9); // status
-            sendNibble(0x4); // data
-            sendNibble(0x1); // data
-            sendNibble(0x5); // checksum
-            delay(10);
-            // Tape Playback
-            sendNibble(0x8); // for head unit
-            sendNibble(0xB); // detailed status
-            sendNibble(0x9); // data
-            sendNibble(0x0); // data
-            sendNibble(0x4); // data
-            sendNibble(0x0); // data
-            sendNibble(0x0); // data
-            sendNibble(0x1); // data
-            sendNibble(0x0); // checksum
-            Serial.println("Init Tape/MD");
+    if ((millis() - rx_time_ms) > RX_TIMEOUT_MS)
+    {
+        if (nibblesReceived != 0U)
+        {
+
+            noInterrupts();
+            {
+                DEBUG_PRINT("RX[");
+                DEBUG_PRINT(nibblesReceived);
+                DEBUG_PRINT("] ");
+
+                for (int i = 0; i < nibblesReceived; i++)
+                {
+                    DEBUG_PRINT(inNibblesBuffer[i], HEX);
+                }
+                DEBUG_PRINT("\r\n");
+
+                process_radio_message((rxMessage_t *)inNibblesBuffer);
+
+                bufferReset();
+
+                rx_time_ms = millis();
+            }
+            interrupts();
         }
-        else if (buff == 0x01)
-        { // Some control command
-            delay(10);
-            // Tape Playing
-            sendNibble(0x8); // for head unit
-            sendNibble(0x9); // status
-            sendNibble(0x4); // data
-            sendNibble(0x1); // data
-            sendNibble(0x5); // checksum
-            delay(10);
-            // Tape Playback
-            sendNibble(0x8); // for head unit
-            sendNibble(0xB); // detailed status
-            sendNibble(0x9); // data
-            sendNibble(0x0); // data
-            sendNibble(0x4); // data
-            sendNibble(0x0); // data
-            sendNibble(0x0); // data
-            sendNibble(0x1); // data
-            sendNibble(0x0); // checksum
-            Serial.println("Control command");
+    }
+}
+
+void bufferReset()
+{
+    for (uint8_t i = 0U; i < nibblesReceived; i++)
+    {
+        inNibblesBuffer[i] = 0U;
+    }
+
+    nibblesReceived = 0;
+    biteShiftMask = NIBBLE_RESET_BIT_POS;
+}
+
+void collectInputData()
+{
+    uint32_t elapsed_time = 0;
+
+    // calculate pulse time
+    elapsed_time = micros() - rx_time_us;
+    rx_time_us = micros();
+    rx_time_ms = millis();
+
+    if (digitalRead(TYPE_IO_PIN) == LOW)
+    {
+        return;
+    }
+
+    if ((elapsed_time > BIT_LOW_LEVEL_DURATION_MIN) && (elapsed_time < BIT_LOW_LEVEL_DURATION_MAX))
+    {
+        inNibblesBuffer[nibblesReceived] |= biteShiftMask;
+    }
+
+    biteShiftMask >>= 1U;
+
+    if (biteShiftMask == 0U)
+    {
+        biteShiftMask = NIBBLE_RESET_BIT_POS; // save one nibble to one byte
+        ++nibblesReceived;
+    }
+
+    if (nibblesReceived >= IN_BUFFER_SIZE)
+    {
+        DEBUG_PRINT("Buffer overflow, reset!\r\n");
+        bufferReset();
+    }
+}
+
+static void send_nibble(const uint8_t nibble)
+{
+    uint8_t nibbleShiftMask = 0x08;
+    uint8_t bit_value = 0U;
+
+    while (nibbleShiftMask != 0U)
+    {
+        // Pull the bus down
+        digitalWrite(TYPE_IO_PIN, LOW);
+
+        bit_value = nibble & nibbleShiftMask;
+
+        // Set Logic 0 or 1 time
+        if (bit_value)
+        {
+            delayMicroseconds(1780);
         }
         else
         {
-            buff = 0;
-            buffPos = 0;
+            delayMicroseconds(600);
         }
-        attachInterrupt(0, isr, CHANGE); // re-enable interrupt
-    }
-}
 
-// function to pull bus low
-void dataPullLow(uint8_t pin)
-{
-    pinMode(pin, OUTPUT);
-    digitalWrite(pin, LOW);
-}
-void dataFloat(uint8_t pin)
-{
-    pinMode(pin, INPUT);
-}
+        // Release the bus
+        digitalWrite(TYPE_IO_PIN, HIGH);
 
-void isr()
-{
-    // on pin bus change
-    if (!digitalRead(DATA_PIN))
-    {                       // if bus low
-        lowTime = micros(); // store time when bus went low
-        if ((micros() - highTime) > 7500)
-        { // clear everything if its been over 7.5ms since last pulse
-            buff = 0;
-            buffPos = 0;
-        }
-    }
-    else
-    { // if bus high
-        if (abs((int)(micros() - lowTime) - 1700) < 300)
-        {                               // if bus was low for about 1.7 ms
-            buff |= 1 << (7 - buffPos); // write 1 to that bit
-        }
-        buffPos++; // increment buffer position no matter what
-        highTime = micros();
-    }
-}
-
-void sendNibble(uint8_t message)
-{
-    for (int i = 0; i < 4; i++)
-    { // send bits 3-0
-        if (message & 1 << (3 - i))
-        {                            // if bit to transmit is a 1
-            dataPullLow(DATA_PIN);   // pull data pin low
-            delayMicroseconds(1700); // wait for appointed time
-            dataFloat(DATA_PIN);     // let bus go high again
-            delayMicroseconds(1200); // wait for appointed time
+        // End logic pause
+        if (bit_value)
+        {
+            delayMicroseconds(1200);
         }
         else
-        {                            // if bit to transmit is a 0
-            dataPullLow(DATA_PIN);   // pull data pin low
-            delayMicroseconds(500);  // wait for appointed time
-            dataFloat(DATA_PIN);     // let bus go high again
-            delayMicroseconds(2400); // wait for appointed time
+        {
+            delayMicroseconds(2380);
         }
+
+        nibbleShiftMask >>= 1U;
+    }
+}
+
+// Send a message on the Mazda radio bus
+void send_message(const uint8_t *message, const uint8_t lenght)
+{
+    DEBUG_PRINT("TX[");
+    DEBUG_PRINT(lenght);
+    DEBUG_PRINT("] ");
+
+    for (int i = 0; i < lenght; i++)
+    {
+        DEBUG_PRINT(((uint8_t *)message)[i], HEX);
+    }
+
+    DEBUG_PRINT("\r\n");
+
+    noInterrupts();
+    {
+
+        do
+        {
+            delay(10);
+        } while (digitalRead(TYPE_IO_PIN) != HIGH);
+
+        detachInterrupt(digitalPinToInterrupt(TYPE_IO_PIN));
+        digitalWrite(TYPE_IO_PIN, HIGH);
+        pinMode(TYPE_IO_PIN, OUTPUT);
+
+        for (uint8_t i = 0; i < lenght; i++)
+        {
+            send_nibble(message[i]);
+        }
+
+        pinMode(TYPE_IO_PIN, TYPE_IO_PIN_INPUT_MODE);
+        attachInterrupt(digitalPinToInterrupt(TYPE_IO_PIN), collectInputData, CHANGE);
+    }
+    interrupts();
+}
+
+void process_radio_message(const rxMessage_t *message)
+{
+    // check target, 0 is tape desk
+    if (message->target != Target_TapeDesk)
+    {
+        return;
+    }
+
+    switch (message->command)
+    {
+    case Command_AnyBodyHome:
+        DEBUG_PRINT("Any body home msg\r\n");
+
+        send_message(TAPECMD_POWER_ON, sizeof(TAPECMD_POWER_ON));
+        send_message(TAPECMD_CASSETE_PRESENT, sizeof(TAPECMD_CASSETE_PRESENT));
+        break;
+    case Command_WakeUp:
+        DEBUG_PRINT("Wake up msg\r\n");
+
+        send_message(TAPECMD_CASSETE_PRESENT, sizeof(TAPECMD_CASSETE_PRESENT));
+        send_message(TAPECMD_STOPPED, sizeof(TAPECMD_STOPPED));
+        break;
+    case Command_Control:
+        if (message->data[0] == SubCommand_Playback)
+        {
+            uint8_t subCmd = ((message->data[1] << 4U) & 0xF0) | (message->data[2] & 0x0F);
+            if (subCmd == Playback_Play)
+            {
+                DEBUG_PRINT("Playback MSG = Playback_Play\r\n");
+                send_message(TAPECMD_PLAYING, sizeof(TAPECMD_PLAYING));
+                send_message(TAPECMD_PLAYBACK, sizeof(TAPECMD_PLAYBACK));
+            }
+            else if (subCmd == Playback_FF)
+            {
+                DEBUG_PRINT("Playback MSG = Playback_FF\r\n");
+                send_message(TAPECMD_PLAYBACK, sizeof(TAPECMD_PLAYBACK));
+            }
+            else if (subCmd == Playback_REW)
+            {
+                DEBUG_PRINT("Playback MSG = Playback_REW\r\n");
+                send_message(TAPECMD_PLAYBACK, sizeof(TAPECMD_PLAYBACK));
+            }
+            else if (subCmd == Playback_Stop)
+            {
+                DEBUG_PRINT("Playback MSG = Playback_Stop\r\n");
+                send_message(TAPECMD_STOPPED, sizeof(TAPECMD_STOPPED));
+            }
+            else
+            {
+                DEBUG_PRINT("Playback MSG = ");
+                DEBUG_PRINT(subCmd);
+                DEBUG_PRINT("\r\n");
+            }
+        }
+        else if (message->data[0] == SubCommand_SeekTrack)
+        {
+            DEBUG_PRINT("SubCommand_SeekTrack\r\n");
+        }
+        else if (message->data[0] == SubCommand_SetConfig)
+        {
+            uint8_t subCmd = ((message->data[1] << 4U) & 0xF0) | (message->data[2] & 0x0F);
+            if (subCmd == SetConfig_RepeatMode)
+            {
+                DEBUG_PRINT("SetConfig_RepeatMode\r\n");
+            }
+            else if (subCmd == SetConfig_RandomMode)
+            {
+                DEBUG_PRINT("SetConfig_RandomMode\r\n");
+            }
+            else if (subCmd == SetConfig_FastForwarding)
+            {
+                DEBUG_PRINT("SetConfig_FastForwarding\r\n");
+            }
+            else if (subCmd == SetConfig_FastRewinding)
+            {
+                DEBUG_PRINT("SetConfig_FastRewinding\r\n");
+            }
+            else
+            {
+                DEBUG_PRINT("SubCommand_SetConfig = ");
+                DEBUG_PRINT(subCmd);
+                DEBUG_PRINT("\r\n");
+            }
+        }
+        else
+        {
+            DEBUG_PRINT("UNCKNOWN Sub command\r\n");
+        }
+        break;
+    default:
+        DEBUG_PRINT("another cmd = ");
+        DEBUG_PRINT(message->command);
+        DEBUG_PRINT("\r\n");
+        break;
     }
 }
